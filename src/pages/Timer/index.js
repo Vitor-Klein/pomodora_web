940455fac377f5807383f244f8be877ca5033ec8
Vitor Klein <vitorhklein2@gmail.com>
2020-05-06 22:01:59 -0300

    upgrade na estilização da pagina Timer

--- src/pages/Timer/index.js
+++ src/pages/Timer/index.js
@@ -1,22 +1,53 @@
 import React, { useState, useRef } from 'react';
-import { CircularProgressbar } from 'react-circular-progressbar'
+import { CircleProgress } from 'react-gradient-progress'
 import { MdPause, MdPlayArrow } from 'react-icons/md'
 
 import './styles.css'
 
+function formatSeconds(seconds) {
+  if (seconds < 60) {
+    return `${seconds} seg`
+  }
+  return `${Math.floor(seconds / 60)} min`
+}
+
 function Timer() {
-  var percentage = 0;
- function TimerContage() {
-   setInterval(() => {
-     percentage = percentage + 1
-   }, 1000)
- }
+  const timerRef = useRef()
+
+  const [timerEnabled, setTimerEnabled] = useState(false)
+  const [secondsEllapsed, setSecondsEllapsed] = useState(0)
   
+  function toggleTimer() {
+
+    if (timerEnabled) {
+      clearInterval(timerRef.current)
+
+      setTimerEnabled(false)
+    } else {
+      timerRef.current = setInterval(() => {
+        setSecondsEllapsed(state => state + 1)
+      }, 100)
+
+      setTimerEnabled(true)
+    }  
+  }
   return (
    <div className="container">   
     <h1 className="title">Pomodora</h1>
-    <CircularProgressbar value={TimerContage()} text={`${percentage}%`} />; 
-    <button className="button"></button>
+    <CircleProgress 
+      percentage={formatSeconds(secondsEllapsed)}
+      primaryColor={['#000000', '#000000']} 
+      secondaryColor={"#fff"}
+      strokeWidth={20}
+      width={350}
+      fontColor={"#fff"}
+      
+    />
+    <button 
+      className="button" 
+      onClick={toggleTimer} 
+      name={timerEnabled ? <MdPause /> : <MdPlayArrow />}
+    />
   </div>
   );
 }
